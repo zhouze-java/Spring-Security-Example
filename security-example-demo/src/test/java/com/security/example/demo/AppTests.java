1bb310fdbb5e4cb8ddc9772130da4e264e031384
zhouze <2909398929@qq.com>
2019-08-02 22:35:04 +0800

    初始化项目

--- security-example-demo/src/test/java/com/security/example/demo/AppTests.java
+++ security-example-demo/src/test/java/com/security/example/demo/AppTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SecurityExampleDemoApplicationTests {
+public class AppTests {
 
     @Test
     public void contextLoads() {
