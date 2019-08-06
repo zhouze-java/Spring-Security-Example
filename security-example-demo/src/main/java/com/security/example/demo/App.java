2b5684f2a6196cfbd2424473cdb922e2e1437431
zhouze <2909398929@qq.com>
2019-08-06 10:51:22 +0800

    扫描其他工程中的配置

--- security-example-demo/src/main/java/com/security/example/demo/App.java
+++ security-example-demo/src/main/java/com/security/example/demo/App.java
@@ -2,8 +2,14 @@ package com.security.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * @author zhouze
+ * @desc ComponentScan 配置父级包, 否则无法扫描到其他工程里面的配置
+ */
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.security.example"})
 public class App {
 
     public static void main(String[] args) {
