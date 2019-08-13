ebe73e6ff2ea649a1a1d0beb41209be5b93e91fd
zhouze <2909398929@qq.com>
2019-08-13 22:11:18 +0800

    短信验证码发送模块重构

--- security-example-core/src/main/java/com/security/example/core/validate/controller/ValidateCodeController.java
+++ security-example-core/src/main/java/com/security/example/core/validate/controller/ValidateCodeController.java
@@ -1,24 +1,13 @@
 package com.security.example.core.validate.controller;
 
-import com.security.example.core.config.SecurityProperties;
-import com.security.example.core.config.redis.Constants;
-import com.security.example.core.validate.code.ValidateCodeGenerator;
 import com.security.example.core.validate.code.ValidateCodeProcessor;
-import com.security.example.core.validate.code.image.ImageCode;
-import com.security.example.core.validate.code.image.ValidateCode;
-import com.security.example.core.validate.code.sms.SmsCode;
-import com.security.example.core.validate.code.sms.SmsCodeSender;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.ServletRequestBindingException;
-import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
