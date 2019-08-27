979ef7dd0dfc3952b7be318442c8275336b66dd6
zhouze <2909398929@qq.com>
2019-08-27 15:41:31 +0800

    资源服务器配置 用户名密码登录重构

--- security-example-app/src/main/java/com/security/example/app/config/MyResourcesServerConfig.java
+++ security-example-app/src/main/java/com/security/example/app/config/MyResourcesServerConfig.java
@@ -1,7 +1,23 @@
 package com.security.example.app.config;
 
+import com.security.example.core.authentication.sms.config.SmsAuthenticationSecurityConfig;
+import com.security.example.core.config.SecurityConstants;
+import com.security.example.core.config.SecurityProperties;
+import com.security.example.core.validate.config.ValidateCodeSecurityConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.session.InvalidSessionStrategy;
+import org.springframework.security.web.session.SessionInformationExpiredStrategy;
+import org.springframework.social.security.SpringSocialConfigurer;
+
+import javax.sql.DataSource;
 
 /**
  * @author 周泽
@@ -10,6 +26,66 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
  */
 @Configuration
 @EnableResourceServer
-public class MyResourcesServerConfig {
+public class MyResourcesServerConfig extends ResourceServerConfigurerAdapter {
 
+    @Autowired
+    private SecurityProperties securityProperties;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private SmsAuthenticationSecurityConfig smsAuthenticationSecurityConfig;
+
+    @Autowired
+    private ValidateCodeSecurityConfig validateCodeSecurityConfig;
+
+    @Autowired
+    private SpringSocialConfigurer securitySocialConfigurer;
+
+    @Autowired
+    private AuthenticationSuccessHandler myAuthenticationSuccessHandler;
+
+    @Autowired
+    private AuthenticationFailureHandler myAuthenticationFailureHandler;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        // web网页登录的配置
+        http.formLogin()
+                .loginPage(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL)
+                .loginProcessingUrl(SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_FORM)
+                .successHandler(myAuthenticationSuccessHandler)
+                .failureHandler(myAuthenticationFailureHandler);
+
+        http
+//                .apply(validateCodeSecurityConfig)
+//                .and()
+                .apply(smsAuthenticationSecurityConfig)
+                .and()
+                .apply(securitySocialConfigurer)
+                .and()
+                .authorizeRequests()
+                // 匹配的是登录页的话放行
+                .antMatchers(
+                        SecurityConstants.DEFAULT_UNAUTHENTICATION_URL,
+                        SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_MOBILE,
+                        SecurityConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX + "/*",
+                        securityProperties.getBrowser().getLoginPage(),
+                        SecurityConstants.DEFAULT_SIGN_UP_URL,
+                        securityProperties.getBrowser().getSignUpPage(),
+                        SecurityConstants.GET_SOCIAL_USER_URL,
+                        securityProperties.getBrowser().getSession().getSessionInvalidUrl(),
+                        "/user/register"
+                )
+                .permitAll()
+                // 授权请求. anyRequest 就表示所有的请求都需要权限认证
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable()
+        ;
+    }
 }
--- security-example-app/src/main/java/com/security/example/app/handler/MyAuthenticationSuccessHandler.java
+++ security-example-app/src/main/java/com/security/example/app/handler/MyAuthenticationSuccessHandler.java
@@ -4,8 +4,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.security.example.core.config.SecurityProperties;
 import com.security.example.core.enums.LoginResponseType;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.UnapprovedClientAuthenticationException;
+import org.springframework.security.oauth2.provider.*;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -27,19 +35,75 @@ public class MyAuthenticationSuccessHandler  extends SavedRequestAwareAuthentica
     private ObjectMapper objectMapper;
 
     @Autowired
+    private ClientDetailsService clientDetailsService;
+
+    @Autowired
+    private AuthorizationServerTokenServices authorizationServerTokenServices;
+
+    @Autowired
     private SecurityProperties securityProperties;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         log.info("用户登录成功...");
 
-        if (LoginResponseType.JSON.equals(securityProperties.getBrowser().getLoginResponseType())) {
-            response.setContentType("application/json;charset=UTF-8");
-            response.getWriter().write(objectMapper.writeValueAsString(authentication));
-        } else {
-            super.onAuthenticationSuccess(request, response, authentication);
+        String header = request.getHeader("Authorization");
+
+        if (header == null || !header.startsWith("Basic ")) {
+            throw new UnapprovedClientAuthenticationException("请在header中传入client信息");
         }
 
+
+        String[] tokens = extractAndDecodeHeader(header, request);
+        assert tokens.length == 2;
+
+        String clientId = tokens[0];
+        String clientSecret = tokens[1];
+
+        // 拿到client的信息之后,去构建clientDetails
+        ClientDetails clientDetails = clientDetailsService.loadClientByClientId(clientId);
+        // 验证
+        if (clientDetails == null) {
+            throw new UnapprovedClientAuthenticationException("clientId错误");
+        } else if (!StringUtils.equals(clientDetails.getClientSecret(), clientSecret)) {
+            throw new UnapprovedClientAuthenticationException("clientSecret不匹配");
+        }
+
+        // 创建 TokenRequest对象
+        TokenRequest tokenRequest = new TokenRequest(MapUtils.EMPTY_MAP, clientId, clientDetails.getScope(), "custom");
+
+        OAuth2Request oAuth2Request = tokenRequest.createOAuth2Request(clientDetails);
+
+        OAuth2Authentication oAuth2Authentication = new OAuth2Authentication(oAuth2Request, authentication);
+
+        OAuth2AccessToken accessToken = authorizationServerTokenServices.createAccessToken(oAuth2Authentication);
+
+        // 返回
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().write(objectMapper.writeValueAsString(accessToken));
+    }
+
+    private String[] extractAndDecodeHeader(String header, HttpServletRequest request)
+            throws IOException {
+
+        byte[] base64Token = header.substring(6).getBytes("UTF-8");
+        byte[] decoded;
+        try {
+            decoded = Base64.decode(base64Token);
+        }
+        catch (IllegalArgumentException e) {
+            throw new BadCredentialsException(
+                    "Failed to decode basic authentication token");
+        }
+
+        String token = new String(decoded, "UTF-8");
+
+        int delim = token.indexOf(":");
+
+        if (delim == -1) {
+            throw new BadCredentialsException("Invalid basic authentication token");
+        }
+        return new String[] { token.substring(0, delim), token.substring(delim + 1) };
     }
 
 }
